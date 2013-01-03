f1b9dba491f627de83b3d3f5059b75c2999dea4f
Urs Breu <dev@gleisdrei.ch>
2013-01-03 12:23:35 +0100

    Restructured build script: - defined common utility tasks 'depends' and 'properties' - additional verbosity for the test task - defined common logging dependencies & switched to logback framework - moved example test case to correct source folder - run build instead of check on ci build

--- backend/src/test/java/org/toadally/model/TaskTest.java
+++ backend/src/test/java/org/toadally/model/TaskTest.java
@@ -1,16 +1,20 @@
 package org.toadally.model;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TaskTest {
 
+	Logger LOGGER = LoggerFactory.getLogger(TaskTest.class.getName());
+	
 	@Test
 	public void test() {
+		LOGGER.debug("starting test");
 		Task parent = new Task();
-		
 		Task child = new Task();
 		child.setParentTask(parent);
 		
